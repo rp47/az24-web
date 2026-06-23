88cf86ee29667315be8eb0b6aa957738365dffb7
rp47 <successorraki25@gmail.com>
2026-06-23 12:52:52 +0530

    chng

--- pub.ps1
+++ pub.ps1
@@ -1,8 +1,10 @@
 $RG = "rg-204"
 $APP = "RPAZ204"
 
-az resource update --resource-group $RG --namespace Microsoft.Web --resource-type basicPublishingCredentialsPolicies --name scm --parent "sites/$APP" --set properties.allow=true
-
-$p = az webapp deployment list-publishing-profiles --resource-group $RG --name $APP --xml
+$p = az webapp deployment list-publishing-profiles `
+  --resource-group $RG `
+  --name $APP `
+  --slot staging `
+  --xml
 
 gh secret set AzureAppService_PublishProfile_9304dd6cd3434095b81ba458d8e814a4 --body "$p"
