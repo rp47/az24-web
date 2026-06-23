6b2bd887af644d42014b2fdc91e1172d14abd1a6
rp47 <successorraki25@gmail.com>
2026-06-23 10:14:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.port || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello from 204');
+});
+
+app.get('/health', (req, res) =>
+    res.json({ status: 'ok', time: new Date().toISOString() })
+);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
